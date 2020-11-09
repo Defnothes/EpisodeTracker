86e038af9aa888732939cd06d490f960b849cee5
Defnothes <74141258+Defnothes@users.noreply.github.com>
2020-11-09 20:37:36 +0900

    Added Time functionality and Download UI

--- ContainerEntry.gd
+++ ContainerEntry.gd
@@ -10,38 +10,61 @@ var watched = []
 var date
 var parent = null
 
+var progress_containers = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_epcount(12)
 	date = ''
 
 func set_properties(title, date, epcount, 
-					watched,
 					callback_object):
 	$LabelTitle.text = title
 	self.date = date
 	self.watched = watched
+	set_epcount(epcount)
 	$LabelTitle.connect("pressed", callback_object, 'fill_details', 
 						[$LabelTitle.text, date, len(episodes)])
 	parent = callback_object
+	set_time_remaining()
+	
 func set_epcount(new_epcount):
 	for i in range(len(episodes),new_epcount):
 		var status_button = preload("res://ButtonProgress.tscn").instance()
-		self.add_child(status_button)
+		get_progress_container(i, new_epcount).add_child(status_button)
+		status_button.index = i
 		status_button.set_watched_callback(self, 'set_watched_episode')
 		episodes.append(status_button)
+		check_new_releases()
+		if (i+1)%4==0 and i!=new_epcount-1:
+			var separator = VSeparator.new()
+			get_progress_container(i, new_epcount).add_child(separator)
 	#for i in range(epcount-new_epcount,epcount): -- should be manual reduction
 	#	remove
+
+func get_progress_container(index, limit):
+	var container_index = 0 if limit<16 else index / 12
+	while container_index >= len(progress_containers):
+		var hcontainer = HBoxContainer.new()
+		$ContainerSeasons.add_child(hcontainer)
+		progress_containers.append(hcontainer)
+	return progress_containers[container_index]
+
+func check_new_releases():
+	for ep in episodes:
+		if ep.current_status == Global.status.UNRELEASED and \
+				Global.is_available_by_time(date, ep.index):
+			ep.set_status(Global.status.RELEASED, ep.index)
 
 func set_episode(ep, stat, watch_action, dl_action):
 	ep = ep-1
 	if ep is int:
-		if ep > len(episodes):
-			set_epcount(ep)
+		if ep >= len(episodes):
+			set_epcount(ep+1)
 		episodes[ep].set_status(stat, ep)
 		episodes[ep].set_watch_callback(watch_action)
 		episodes[ep].set_download_callback(dl_action)
-		
+	
+	
 func set_watched_episode(episode, already_watched):
 	if not already_watched:
 		watched.erase(episode)
@@ -54,3 +77,15 @@ func update_watched_list(watched_eps):
 	for ep in watched_eps:
 		if ep<len(episodes):
 			episodes[ep].set_status(Global.status.WATCHED,ep)
+			
+func set_time_remaining():
+	var time_remaining = 9223372036854775807
+	var time_prev = 9223372036854775807
+	for ep in episodes:
+		time_prev = time_remaining
+		time_remaining = Global.seconds_until(date) - 7*24*3600*ep.index
+		if time_prev>=0 and time_remaining<0:
+				break
+			
+	
+	$LabelTimeRemaining.set_text(Global.seconds_to_string(-time_remaining))
--- global.gd
+++ global.gd
@@ -21,17 +21,86 @@ var status2icon = {
 }
 
 var default_epnr_regex : RegEx
+var time_regex : RegEx
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	default_epnr_regex = RegEx.new()
+	time_regex = RegEx.new()
 	var _err = default_epnr_regex.compile('- (\\d\\d)')
+	_err = time_regex.compile('(\\d+)[\\./](\\d+)[\\./](\\d+) (\\d+):(\\d+)')
+	
 	for key in status2icon.keys():
 		var im = status2icon[key].get_data()
 		im.resize(16,16)
 		status2icon[key] = ImageTexture.new()
 		status2icon[key].create_from_image(im)
 	
+func unix_time_from_string(time_string):
+	time_string += (' 1:00')
+	var time_res = time_regex.search(time_string)
+	if time_res == null:
+		print('Invalid date string')
+		return null
+		
+	var time_dict = {'year': time_res.get_string(1), 
+					'month' : time_res.get_string(2), 
+					'day' : time_res.get_string(3),
+					'hour': time_res.get_string(4),
+					'minute': time_res.get_string(5)}
+	if int(time_dict['year']) <100:
+		time_dict['year'] = '20' + time_dict['year']
+	
+	return OS.get_unix_time_from_datetime(time_dict)
+
+func is_available_by_time(release_date, weeks_passed, pt=false):
+	var secs_left = seconds_until(release_date)
+	if pt:
+		#print(OS.get_time_zone_info()['bias'])
+		print (secs_left,
+		' : ',secs_left/3600.0/24.0/7.0,
+		 ' ', weeks_passed)
+	return secs_left/3600.0/24.0/7.0>=weeks_passed
+
+func seconds_until(release_date):
+	if release_date == null:
+		print('The release date is invalid')
+		return false
+	var date_to_unix = Global.unix_time_from_string(release_date)
+	if date_to_unix == null:
+		print('The release date is invalid')
+		return false
+		
+	#This compensates for GMTK and timezones.
+	var current_time = OS.get_unix_time_from_datetime(OS.get_datetime())
+
+	return current_time - date_to_unix
+	
+func seconds_to_string(time_seconds):
+	time_seconds += 0
+	var time :String
+	var minutes_left = time_seconds / 60
+	var hrs_left =  time_seconds/ 3600
+	var days_left = hrs_left / 24
+	
+	if time_seconds<0:
+		time = '-'
+	elif days_left>0:
+		time = str(time_seconds/24/3600) 
+		if days_left>1:
+			time += ' days'
+		else:
+			time += ' day'
+	elif hrs_left>0:
+		time += str(hrs_left)
+		if hrs_left>1:
+			time+= ' hrs '
+		else:
+			time += 'hr '
+	elif minutes_left>=0:
+		time += str(minutes_left)
+		time += ' min'
+	return time
 
 func list_files_in_directory(path):
 	var file_list = []
